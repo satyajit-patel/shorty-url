f768986daed7649076b59140df46816175f333a5
satyajit-patel <satyajitpatel770001@gmail.com>
2025-04-27 00:29:39 +0530

    freaking bug [id]

--- src/app/table/[id]/page.tsx
+++ src/app/table/[id]/page.tsx
@@ -1,17 +1,18 @@
 import { redirect } from "next/navigation";
 import { getUrlByShortUrl } from "@/controllers/page";
 
-type PageProps = { params: { id: string } };
+export default async function urlRedirect({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
+  const shortUrl = `table/${id}`;
+  const response = await getUrlByShortUrl(shortUrl);
 
-export default async function urlRedirect({params}: PageProps) {
-    const {id} = params;
-    const shortUrl = "table" + "/" + id;
-    // console.log("*****", shortUrl, "************");
-    const response = await getUrlByShortUrl(shortUrl);
-    // console.log(response);
-    if(response && response.originalUrl) {
-        redirect(response.originalUrl);
-    } else {
-        redirect("/404");
-    }
+  if (response?.originalUrl) {
+    redirect(response.originalUrl);
+  } else {
+    redirect("/404");
+  }
 }
