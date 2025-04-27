563cd44de17e71e98db65accff7d7d13d6c62975
satyajit-patel <satyajitpatel770001@gmail.com>
2025-04-27 11:18:55 +0530

    bugfix added promise in params

--- src/app/table/[id]/page.tsx
+++ src/app/table/[id]/page.tsx
@@ -1,12 +1,12 @@
 import { redirect } from "next/navigation";
 import { getUrlByShortUrl } from "@/controllers/page";
 
-export default async function urlRedirect({ params }: any) {
+export default async function urlRedirect({ params }: {params: Promise<{ id: string }>}) {
   const { id } = await params;
   const shortUrl = `table/${id}`;
   const response = await getUrlByShortUrl(shortUrl);
-  // console.log("******************************************************************************************");
-  // console.log(response);
+  console.log("******************************************************************************************");
+  console.log(response);
 
   if (response?.originalUrl) {
     redirect(response.originalUrl);
